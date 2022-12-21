1961da17b2f4e2bf1d19e496f1c9457affe474a0
Hoodinii890 <jhormangallegogallego@gmail.com>
2022-12-21 09:10:40 -0500

    pull

--- ModSubcontratos/urls.py
+++ ModSubcontratos/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path("", login_required(SubContratos.as_view()), name="subcontratos"),
+    path("",login_required(SubContratos.as_view()), name="subcontratos"),
     path("GuardarSubontrato/", login_required(GuardarSubcontrato.as_view()), name="guardarSubcontrato"),
 ]
 
